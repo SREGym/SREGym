0e624b1b2a21942ad34eed3edb475a86892ac917
Saleha <saleha@192.168.1.9>
2025-06-01 01:23:59 +0500

    fix changes for cloudlab

--- aiopslab/remote.py
+++ aiopslab/remote.py
@@ -1,7 +1,10 @@
 import paramiko
 
+
 class RemoteExecutor:
-    def __init__(self, host, user, key_path):
+    """Thin SSH helper around paramiko suitable for non-interactive commands."""
+
+    def __init__(self, host: str, user: str, key_path: str):
         self.host = host
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -9,12 +12,18 @@ class RemoteExecutor:
             hostname=host,
             username=user,
             key_filename=key_path,
+            look_for_keys=False,
+            allow_agent=False,
         )
 
-    def exec(self, cmd):
+    def exec(self, cmd: str) -> tuple[int, str, str]:
+        """
+        Execute *cmd* and return (exit_code, stdout, stderr).
+        All outputs are decoded as UTF-8.
+        """
         stdin, stdout, stderr = self.client.exec_command(cmd)
-        code = stdout.channel.recv_exit_status()
-        return code, stdout.read().decode(), stderr.read().decode()
+        exit_code = stdout.channel.recv_exit_status()
+        return exit_code, stdout.read().decode(), stderr.read().decode()
 
-    def close(self):
+    def close(self) -> None:
         self.client.close()
