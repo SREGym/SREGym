5173912c48d605a19518fd9f74da9d7377d5a4f8
Jackcuii <forever_1210@yeah.net>
2025-08-21 11:46:00 -0500

    make a demo for the key change

--- srearena/conductor/problems/taint_no_toleration.py
+++ srearena/conductor/problems/taint_no_toleration.py
@@ -8,14 +8,13 @@ from srearena.utils.decorators import mark_fault_injected
 
 
 class TaintNoToleration(Problem):
+    required_config = ["faulty_service", "faulty_nodes"]
+    
     def __init__(self):
         self.app = SocialNetwork()
         self.namespace = self.app.namespace
         self.kubectl = KubeCtl()
-
-        # ── pick all real worker nodes dynamically ───────────────────────
-        self.faulty_nodes = self._pick_worker_nodes()
-        self.faulty_service = "user-service"
+        self.config = {}
 
         super().__init__(app=self.app, namespace=self.namespace)
 
@@ -41,16 +40,16 @@ class TaintNoToleration(Problem):
 
     @mark_fault_injected
     def inject_fault(self):
-        print(f"Injecting Fault to Service {self.faulty_service} on Nodes {self.faulty_nodes}")
-        for node in self.faulty_nodes:
+        print(f"Injecting Fault to Service {self.config['faulty_service']} on Nodes {self.config['faulty_nodes']}")
+        for node in self.config['faulty_nodes']:
             self.kubectl.exec_command(f"kubectl taint node {node} sre-fault=blocked:NoSchedule --overwrite")
 
         patch = """[{"op": "add", "path": "/spec/template/spec/tolerations",
                      "value": [{"key": "dummy-key", "operator": "Exists", "effect": "NoSchedule"}]}]"""
         self.kubectl.exec_command(
-            f"kubectl patch deployment {self.faulty_service} -n {self.namespace} --type='json' -p='{patch}'"
+            f"kubectl patch deployment {self.config['faulty_service']} -n {self.namespace} --type='json' -p='{patch}'"
         )
-        self.kubectl.exec_command(f"kubectl delete pod -l app={self.faulty_service} -n {self.namespace}")
+        self.kubectl.exec_command(f"kubectl delete pod -l app={self.config['faulty_service']} -n {self.namespace}")
 
     @mark_fault_injected
     def recover_fault(self):
@@ -58,3 +57,19 @@ class TaintNoToleration(Problem):
         # assuming recover_toleration_without_matching_taint can accept multiple services and a node list
         for node in self.faulty_nodes:
             self.injector.recover_toleration_without_matching_taint([self.faulty_service], node_name=node)
+
+
+    def deploy_fault_config(self, config):
+        self.config = config
+        
+    def init_config_iteration(self):
+        # get all the deployments in the namespace
+        deployments = self.kubectl.core_v1_api.list_deployment(namespace=self.namespace).items
+        for deployment in deployments:
+            if deployment.spec.template.spec.tolerations:
+                self.candidate_deployments.append(deployment.metadata.name)
+        
+    def iterate_fault_config(self):
+        self.config["faulty_nodes"] = self._pick_worker_nodes()
+        self.config["faulty_service"] = self.candidate_deployments.pop(0)
+        
