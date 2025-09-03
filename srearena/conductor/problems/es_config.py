799d2aa5ef414f8e2742dd0bdabdc541468cd875
Edirent <sz73@illinois.edu>
2025-09-03 01:14:37 -0400

    Add MisconfigAppEsSearch class for fault injection and recovery in Elasticsearch

---
+++ srearena/conductor/problems/es_config.py
@@ -0,0 +1,49 @@
+"""MongoDB storage user unregistered problem in the HotelReservation application."""
+
+from srearena.conductor.oracles.compound import CompoundedOracle
+from srearena.conductor.oracles.localization import LocalizationOracle
+from srearena.conductor.oracles.mitigation import MitigationOracle
+from srearena.conductor.oracles.workload import WorkloadOracle
+from srearena.conductor.problems.base import Problem
+from srearena.generators.fault.inject_app import ApplicationFaultInjector
+from srearena.service.apps.elasticsearch import Elasticsearch
+from srearena.service.kubectl import KubeCtl
+from srearena.utils.decorators import mark_fault_injected
+
+
+class MisconfigAppEsSearch(Problem):
+    def __init__(self):
+        self.app = Elasticsearch()
+        self.kubectl = KubeCtl()
+        self.namespace = self.app.namespace
+        self.faulty_service = "geo"
+        super().__init__(app=self.app, namespace=self.app.namespace)
+        # === Attach evaluation oracles ===
+        self.localization_oracle = LocalizationOracle(problem=self, expected=[self.faulty_service])
+
+        self.app.create_workload()
+        self.mitigation_oracle = CompoundedOracle(
+            self,
+            MitigationOracle(problem=self),
+            WorkloadOracle(problem=self, wrk_manager=self.app.wrk),
+        )
+
+    @mark_fault_injected
+    def inject_fault(self):
+        print("== Fault Injection ==")
+        injector = ApplicationFaultInjector(namespace=self.namespace)
+        injector._inject(
+            fault_type="misconfig_app",
+            microservices=[self.faulty_service],
+        )
+        print(f"Service: {self.faulty_service} | Namespace: {self.namespace}\n")
+
+    @mark_fault_injected
+    def recover_fault(self):
+        print("== Fault Recovery ==")
+        injector = ApplicationFaultInjector(namespace=self.namespace)
+        injector._recover(
+            fault_type="misconfig_app",
+            microservices=[self.faulty_service],
+        )
+        print(f"Service: {self.faulty_service} | Namespace: {self.namespace}\n")
