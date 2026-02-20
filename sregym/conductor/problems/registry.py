798ad74ee2d308a1d70310cb0747c0dab6774233
Yiming Su <yiming34@illinois.edu>
2026-02-20 00:49:55 -0600

    preset percentage for latent sector error

--- sregym/conductor/problems/registry.py
+++ sregym/conductor/problems/registry.py
@@ -171,7 +171,9 @@ class ProblemRegistry:
             # ==================== HARDWARE FAULT INJECTOR ====================
             "silent_data_corruption": SilentDataCorruption,
 
-            "latent_sector_error": lambda: KhaosFaultProblem(KhaosFaultName.latent_sector_error),
+            "latent_sector_error_30": lambda: KhaosFaultProblem(KhaosFaultName.latent_sector_error,inject_args=[30]),
+            "latent_sector_error_50": lambda: KhaosFaultProblem(KhaosFaultName.latent_sector_error,inject_args=[50]),
+            "latent_sector_error_100": lambda: KhaosFaultProblem(KhaosFaultName.latent_sector_error,inject_args=[100]),
             "read_error": lambda: KhaosFaultProblem(KhaosFaultName.read_error),
             # "pread_error": lambda: KhaosFaultProblem(KhaosFaultName.pread_error),
             # "write_error": lambda: KhaosFaultProblem(KhaosFaultName.write_error),
