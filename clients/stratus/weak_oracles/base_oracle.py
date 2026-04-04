3e2343b30c18d558460103ca223948b5f9e43c1d
Yiming Su <66594223+yimingsu01@users.noreply.github.com>
2026-04-04 09:10:08 -0500

    Fixed retry logic in `stratus` mitigation agent (#719)
    * replace submit_tool with f_submit_tool
    
    * we should really submit to the benchmark when oracles pass, and retry if not.
    
    * make sure retry continues
    
    * continues.
    
    * make sure we convert all retry attempts into the html
    
    * make alert results as strings
    
    * ruff check, typo fix
    
    * add string representation to BaseOracle
    
    ---------
    
    Co-authored-by: yimingsu <yimingsu@node8.yimingsu-296614.sieve-acto-pg0.wisc.cloudlab.us>
    Co-authored-by: Pial <saadmrp222@gmail.com>

--- clients/stratus/weak_oracles/base_oracle.py
+++ clients/stratus/weak_oracles/base_oracle.py
@@ -9,11 +9,17 @@ class OracleResult:
         self.success = success
         self.issues = issues
 
-    def __str__(self):
+    def __repr__(self):
         return f"Your last mitigation attempt [{'has succeeded' if self.success else 'has failed'}]. The potential issues are [{'no issues as you have succeeded' if self.success else self.issues}]"
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class BaseOracle(ABC):
     @abstractmethod
     def validate(self, **kwargs) -> OracleResult:
         pass
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}"
