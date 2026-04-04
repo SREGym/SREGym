237225769adebe10cfd854eb99d56d60361a859e
Yiming Su <yiming34@illinois.edu>
2026-04-04 16:59:29 -0500

    restore MS problem mitigation oracle

--- sregym/conductor/problems/load_spike_rpc_retry_storm.py
+++ sregym/conductor/problems/load_spike_rpc_retry_storm.py
@@ -1,3 +1,4 @@
+from sregym.conductor.oracles.alert_oracle import AlertOracle
 from sregym.conductor.oracles.llm_as_a_judge.llm_as_a_judge_oracle import LLMAsAJudgeOracle
 from sregym.conductor.problems.base import Problem
 from sregym.generators.fault.inject_virtual import VirtualizationFaultInjector
