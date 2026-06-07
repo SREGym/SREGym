1f5af1a4ebe419377342e81c4cd22c6248a931be
Pial <saadmrp222@gmail.com>
2026-06-07 17:06:12 +0600

    add fault injection decorator markers to test_problem_bsae.py

--- tests/problems/test_problem_base.py
+++ tests/problems/test_problem_base.py
@@ -1,12 +1,15 @@
 from types import SimpleNamespace
 
 from sregym.conductor.problems.base import Problem
+from sregym.utils.decorators import mark_fault_injected
 
 
 class ExampleProblem(Problem):
+    @mark_fault_injected
     def inject_fault(self):
         pass
 
+    @mark_fault_injected
     def recover_fault(self):
         pass
 
